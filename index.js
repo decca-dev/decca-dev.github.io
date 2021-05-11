7c15c2681bf4ab5a9ca6e0126bda03b35faddf9e
decca_dev <medcherbani@gmail.com>
2021-05-11 16:36:09 +0000

    revamp

---
+++ index.js
@@ -0,0 +1,23 @@
+
+var i = 0;
+var txt = randomText();
+var speed = 50;
+
+document.addEventListener("DOMContentLoaded", function() {
+  typeWriter();
+})
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("typewrite").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+function randomText() {
+  var textArray = ["I am a full-stack developer.", "I make APIs.", "I make discord bots.", "I'm an upcoming UX/UI designer."];
+  var randomNumber = Math.floor(Math.random()*textArray.length);
+
+  return textArray[randomNumber];
+}
